c34178836b520f329d96e481720a1ce34b53dc6d
amaslan <annie@maslan.com>
2021-09-06 16:49:36 -0700

    add shell commands

---
+++ run_bs_pipeline.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=amm_rna
+#
+# Account:
+#SBATCH --account=co_rosalind
+#
+# Partition:
+#SBATCH --partition=savio
+#
+# Quality of Service:
+#SBATCH --qos=rosalind_savio_normal
+#
+# Wall clock limit:
+#SBATCH --time=2-00:00:00
+#
+# Nodes:
+#SBATCH --nodes=1
+## Command(s) to run:
+module load fastqc/0.11.9
+module load samtools/1.8
+module load multiqc/1.9
+snakemake -j --rerun-incomplete
